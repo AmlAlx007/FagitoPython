b8ea376cb1090b2effe27689fad9285b4302f693
AmlAlx007 <amlalx007@gmail.com>
2020-05-21 21:32:17 +0100

    Accessing values from database

--- app/controllers/Register.py
+++ app/controllers/Register.py
@@ -1,5 +1,21 @@
+from flask import request
+from app import db
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+
+
 def signup():
-    return '<h1>SignUp!</h1>'
+    request_data = request.get_json()
+    myDB = URL(drivername='mysql', username='admin', password='fAgitod3v', host='fagitodev.cw0t204dkdrj.eu-west-1.rds.amazonaws.com', database='fagito')
+    engine = create_engine(name_or_url = myDB)
+    connection = engine.connect()
+
+    query = 'SELECT * FROM sign_up'
+
+    result = connection.execute(query)
+    for val in result:
+        print(val)
+    return "hello"
 
 
 def login():
